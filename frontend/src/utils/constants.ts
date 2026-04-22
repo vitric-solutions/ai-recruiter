c9ec28648051c2c94d528acc8269f17b27255fcb
thakarevaibhav28 <144985144+Thakarevaibhav@users.noreply.github.com>
2026-04-22 11:14:40 +0530

    added production url

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,7 +1,10 @@
  //Local
- export const Base_Url = 'http://localhost:3000/api'
-export const Socket_Url = 'http://localhost:3000'
+//  export const Base_Url = 'http://localhost:3000/api'
+// export const Socket_Url = 'http://localhost:3000'
 
  //Production
 //   export const Base_Url = 'http://3.6.50.72:3000/api'
 // export const Socket_Url = 'http://3.6.50.72:3000'
+
+  export const Base_Url = 'https://ai-interview.vitric.in/api'
+export const Socket_Url = 'https://ai-interview.vitric.in'
